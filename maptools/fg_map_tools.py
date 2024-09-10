e6ed6c99c68b4537f0bd94347c4d097c65e60f1c
Andrew Liu <liuyiqiandrew@gmail.com>
2024-09-10 01:59:05 -0400

    add scale-map func for fg map tools

--- maptools/fg_map_tools.py
+++ maptools/fg_map_tools.py
@@ -15,3 +15,15 @@ def create_mbb_scale_array(nus, beta_map, T_d=19.6, nu0=353.):
     scale_arr = create_pl_scale_array(nus=nus, beta_map=beta_map, nu0=nu0)
     scale_arr *= (pygsm.planck_law(T_d, nus) / pygsm.planck_law(T_d, nu0))[:, None]
     return scale_arr
+
+
+def scale_maps(pivot_map, scale_arr, nus, nu0):
+    """ scale the input pivoted map """
+    if pivot_map.shape[0] != 1 and pivot_map.shape[0] != 2:
+        raise RuntimeError(f"Pivot map have axis 0 size {pivot_map.shape[0]}, should be 1 or 2")
+    pivot_map *= pygsm.tcmb2trj(nu0)
+    if pivot_map.shape[0] == 2:
+        scale_arr = np.repeat(scale_arr, repeats=2, axis=0).reshape(nus.shape[0], 2, pivot_map.shape[1])
+    scaled_maps = pivot_map * scale_arr
+    scaled_maps *= pygsm.trj2tcmb(nus)[:, None, None]
+    return scaled_maps
